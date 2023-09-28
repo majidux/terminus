1bdd196c5c4fd3ab8e3f4cac7dc2bca55f5cb2cf
Majid <majid@Majids-MacBook-Air.local>
2023-09-28 16:53:43 +0330

    feat: delete useless module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProvincesModule } from './provinces/provinces.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      port: 5432,
+      username: 'majid',
+      password: 'majid',
+      database: 'terminus',
       entities: [],
       synchronize: true,
     }),
     UsersModule,
-    ProvincesModule,
   ],
 })
 export class AppModule {}
--- src/provinces/provinces.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProvincesService } from './provinces.service';
-import { ProvincesController } from './provinces.controller';
-
-@Module({
-  controllers: [ProvincesController],
-  providers: [ProvincesService],
-})
-export class ProvincesModule {}
