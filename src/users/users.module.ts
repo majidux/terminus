87ac1d5752d10923c94b8978802b26eed2c7310a
majid <m.darvishnejad@emofid.com>
2023-10-01 16:31:44 +0330

    feat: add login

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserModel } from './user.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserModel }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
