ad22ce3a3600baaf13654fdf6f7c43778720d0c8
m.darvishnejad <m.darvishnejad@emofid.com>
2023-12-24 11:34:37 +0330

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,25 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 // import { constants } from './constant';
 // import { ConnectOptions } from 'typeorm';
-import { User } from './users/entities/user.entity';
+// import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
     // MongooseModule.forRoot(constants.dataBaseConnectionString),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      port: 5432,
-      username: 'majid',
-      password: 'majid12345',
-      database: 'terminus',
-      host: 'localhost',
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER?.toString(),
+      password: process.env.PG_PASSWORD?.toString(),
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
-      entities: [User],
       autoLoadEntities: true,
     }),
-    ConfigModule.forRoot(),
     UsersModule,
   ],
 })
