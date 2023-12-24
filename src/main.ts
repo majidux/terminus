4e67e5ace7aa6cf39e09b8dabcdb2fdce034e4aa
Majid Darvishnejad <46152564+majidux@users.noreply.github.com>
2023-12-24 14:49:20 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
